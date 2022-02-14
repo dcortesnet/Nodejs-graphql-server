f2e63550abcdeeb05f707a250b672dbc0d7ad963
Diego Esteban Cortes Escandón <e-dcortes@autopia.cl>
2022-02-14 00:13:17 -0300

    :sparkles: feat: Consultas básicas

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { ApolloServer, gql } from "apollo-server";
+
 const persons = [
   {
     name: "John",
@@ -16,3 +18,33 @@ const persons = [
     id: "123-456-7890",
   },
 ];
+
+const typeDefs = gql`
+  type Person {
+    name: String!
+    phone: String
+    age: Int
+    city: String
+    id: ID!
+  }
+
+  type Query {
+    personsCount: Int
+    getAllPersons: [Person]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    personsCount: () => persons.length,
+    getAllPersons: () => persons,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  {
+    console.log(`🚀  Server ready at ${url}`);
+  }
+});
